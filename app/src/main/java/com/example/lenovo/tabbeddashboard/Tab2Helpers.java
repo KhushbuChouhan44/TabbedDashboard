c6721a47366bfb8654e1818fdaa4582204ce4b0c
Khushbu <khushbuchouhan44@gmail.com>
2018-05-06 16:26:43 +0530

    last commit of domhelp!

---
+++ app/src/main/java/com/example/lenovo/tabbeddashboard/Tab2Helpers.java
@@ -0,0 +1,18 @@
+package com.example.lenovo.tabbeddashboard;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Helpers extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2helpers, container, false);
+
+        return rootView;
+    }
+}
